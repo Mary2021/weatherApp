62c8cd67718e1a70c790d18d68539a628c97fb59
Mary P <mary.pommer@tptlive.ee>
2021-12-09 11:16:04 +0200

    add WeatherApp

---
+++ WeatherApp/Adapters/ForecastAdapter.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WeatherApp.Models;
+
+namespace WeatherApp.Adapters
+{
+    public class ForecastAdapter : BaseAdapter<List>
+    {
+        List<List> _items;
+        Activity _context;
+
+        public ForecastAdapter(Activity context, List<List> items)
+        {
+            _items = items;
+            _context = context;
+        }
+
+        public override List this[int position]
+        {
+            get { return _items[position]; }
+        }
+
+        public override int Count
+        {
+            get { return _items.Count; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            if (view == null)
+                view = _context.LayoutInflater.Inflate(Resource.Layout.day_row_layout, null);
+            view.FindViewById<TextView>(Resource.Id.dayTextView).Text = _items[position].dt_txt;
+            view.FindViewById<TextView>(Resource.Id.dayTemperatureTextView).Text = _items[position].main.temp.ToString() + " °C";
+            view.FindViewById<TextView>(Resource.Id.dayWindTextView).Text = _items[position].wind.speed.ToString() + " m/s";
+            return view;
+        }
+        public void UpdateData(List<List> items)
+        {
+            _items = items;
+        }
+    }
+}
